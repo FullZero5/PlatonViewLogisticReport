97caba370ef5b4edb78b8c4a075ce3edc590a738
@FullZero <fullzero5@gmail.com>
2019-04-24 15:42:13 +0300

    del getters
    delet

--- src/store/getters/index.js
+++ src/store/getters/index.js
@@ -3,11 +3,5 @@ export default {
   getDistanceTraveled: ({ data }) =>
     data.reduce((prev, next) => prev + next.distance, 0).toFixed(2),
   getLengtData: ({ data }) => data.length,
-  getMonth: ({ data }) => data[0].startDate.getMonth(),
-  MonthData: ({ data }) =>
-    new Date(
-      data[0].startDate.getFullYear(),
-      data[0].startDate.getMonth(),
-      1
-    ).toLocaleString("ru", "short")
+  getMonth: ({ data }) => data[0].startDate.getMonth()
 };
